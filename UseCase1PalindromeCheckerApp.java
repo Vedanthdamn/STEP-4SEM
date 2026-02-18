c12f01072ec4fa694119080b3bed244f2526f76c
Vedanthdamn <vedanthdama@gmail.com>
2026-02-18 14:47:55 +0530

    a

---
+++ UseCase1PalindromeCheckerApp.java
@@ -0,0 +1,9 @@
+public class UseCase1PalindromeCheckerApp {
+    public static void main(String[] args) {
+        String appName = "Palindrome Checker Management System";
+        String appVersion = "1.0.0";
+        System.out.println("Welcome to " + appName);
+        System.out.println("Application Version: " + appVersion);
+        System.out.println("System initialized successfully.");
+    }
+}
