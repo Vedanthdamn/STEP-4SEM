70f92c16c1b055b38c978e0cc04dcb7eb5a22f33
Vedanthdamn <vedanthdama@gmail.com>
2026-02-18 15:11:01 +0530

    commit

--- UseCase2PalindromeCheckerApp.java
+++ UseCase2PalindromeCheckerApp.java
@@ -1,24 +1,16 @@
-// UseCase2PalindromeCheckerApp.java
-
 public class UseCase2PalindromeCheckerApp {
-
-    // main method: entry point of the application
     public static void main(String[] args) {
-
-        // hardcoded string literal
         String word = "madam";
-
-        // reverse the string
         String reversed = "";
         for (int i = word.length() - 1; i >= 0; i--) {
             reversed = reversed + word.charAt(i);
         }
-
-        // conditional check: is it a palindrome?
         if (word.equals(reversed)) {
-            System.out.println(word + " is a palindrome");
+            System.out.println("Input text: " + word);
+            System.out.println("Is it a pallindrome: true");
         } else {
-            System.out.println(word + " is not a palindrome");
+            System.out.println("Input text: " + word);
+            System.out.println("Is it a pallindrome: false");
         }
     }
 }
